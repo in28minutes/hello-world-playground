e600eabbacc684207977883298faca1860426db8
Ranga Karanam <rangakaranam@Rangas-MacBook-Air.local>
2019-08-13 19:14:53 +0530

    Thank You for Choosing to Learn from in28Minutes

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -14,7 +14,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World - Changed");
+		return new HelloWorldBean("Hello World - Changed - V2");
 	}
 	
 	@GetMapping(path = "/hello-world/path-variable/{name}")
